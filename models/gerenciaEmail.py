85747d5584a17ad92727e18e6f91c042a7334652
Allan Andrade <allan@urge.space>
2022-01-19 20:40:30 -0300

    Criação inicial dos modulos necessários para a funcionalidade de verificação de vencimentos.

---
+++ models/gerenciaEmail.py
@@ -0,0 +1,14 @@
+
+
+class GerenciadorDeEmail:
+
+    def __init__(self):
+        pass
+
+    # Realiza a operação de conexão com o serviço de e-mail
+    def conectar_email(self):
+        pass
+
+    # Realiza o envio do email
+    def enviar_email(self, params: dict) -> None:
+        pass
---
+++ models/verificaVencimentos.py
@@ -0,0 +1,6 @@
+
+
+class VerificaVencimentoContratos:
+    # Verifica o vencimento dos contratos no banco e retorna uma lista com contratos próximos ao vencimento.
+    def verificar_vencimento(self) -> list:
+        pass
