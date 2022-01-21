78b96fc64f60516ee5347f419fbb9d49dfaba498
Allan Andrade <allan@urge.space>
2022-01-20 23:56:54 -0300

    Bug resolvido na parte de renovação de contrato

--- controller/renovarContrato.py
+++ controller/renovarContrato.py
@@ -9,7 +9,8 @@ class RenovaContrato:
 
     def renovar(self, id_contrato: int, data_vencimento: str, novo_anexo: str) -> int:
             contrato = self.banco.pesquisar_por_id(id_contrato)
-            duracao = contrato[0][8]
+            inicio_vigencia = contrato[0][6]
+            duracao = ConfirmaNovoContrato.retorna_duracao_contrato(inicio_vigencia, data_vencimento)
             situacao = ConfirmaNovoContrato.retorna_situacao_contrato(data_vencimento)
             self.banco.renovar_contrato(id_contrato, data_vencimento, duracao, situacao)
             self.banco.commit()
